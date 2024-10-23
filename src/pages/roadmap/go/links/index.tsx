af259ea6652fd196f93edad86e1fe957f3e4c094
Zrossiz <soon.pluxury@mail.ru>
2024-10-23 13:34:36 +0300

    feat: added go links page

--- src/pages/roadmap/go/links/index.tsx
+++ src/pages/roadmap/go/links/index.tsx
@@ -3,7 +3,101 @@ import { Layout } from '@/layout/Layout';
 const GoLinksPage = () => {
   return (
     <Layout>
-      <div>links</div>
+      <>
+        <h1>Полезные материалы</h1>
+
+        <h2>Проекты</h2>
+        <ul>
+          <li>
+            <strong>Авторизация gRPC:</strong>{' '}
+            <a href="https://www.youtube.com/watch?v=EURjTg5fw-E" target="_blank">
+              https://www.youtube.com/watch?v=EURjTg5fw-E
+            </a>
+          </li>
+          <li>
+            <strong>URL shortener:</strong>{' '}
+            <a href="https://www.youtube.com/watch?v=rCJvW2xgnk0" target="_blank">
+              https://www.youtube.com/watch?v=rCJvW2xgnk0
+            </a>
+          </li>
+        </ul>
+
+        <h2>Собеседования</h2>
+        <ul>
+          <li>
+            <strong>Собес:</strong>{' '}
+            <a href="https://www.youtube.com/watch?v=GD0iHLucYdU" target="_blank">
+              https://www.youtube.com/watch?v=GD0iHLucYdU
+            </a>
+          </li>
+          <li>
+            <strong>Собес:</strong>{' '}
+            <a href="https://www.youtube.com/watch?v=c8Y5PN8_Vcw" target="_blank">
+              https://www.youtube.com/watch?v=c8Y5PN8_Vcw
+            </a>
+          </li>
+        </ul>
+
+        <h2>Go</h2>
+        <ul>
+          <li>
+            <strong>Документация Go:</strong>{' '}
+            <a href="https://go.dev/doc/" target="_blank">
+              https://go.dev/doc/
+            </a>
+          </li>
+          <li>
+            <strong>Устройство планировщика Go:</strong>{' '}
+            <a href="https://www.youtube.com/watch?v=P2Tzdg8n9hw" target="_blank">
+              https://www.youtube.com/watch?v=P2Tzdg8n9hw
+            </a>
+          </li>
+          <li>
+            <strong>Как работают каналы:</strong>{' '}
+            <a href="https://www.youtube.com/watch?v=ZTJcaP4G4JM" target="_blank">
+              https://www.youtube.com/watch?v=ZTJcaP4G4JM
+            </a>
+          </li>
+          <li>
+            <strong>Многопоточность в Go:</strong>{' '}
+            <a href="https://www.youtube.com/watch?v=aYgl7tUwx5k" target="_blank">
+              https://www.youtube.com/watch?v=aYgl7tUwx5k
+            </a>
+          </li>
+          <li>
+            <strong>Пример архитектуры Go проекта:</strong>{' '}
+            <a href="https://www.youtube.com/watch?v=e-D28OoWsOg" target="_blank">
+              https://www.youtube.com/watch?v=e-D28OoWsOg
+            </a>
+          </li>
+        </ul>
+
+        <h2>Базы данных</h2>
+        <ul>
+          <li>
+            <strong>Проектирование БД:</strong>{' '}
+            <a href="https://habr.com/ru/articles/514364/" target="_blank">
+              https://habr.com/ru/articles/514364/
+            </a>
+          </li>
+          <li>
+            <strong>Нормальные формы базы данных (SQL):</strong>{' '}
+            <a href="https://www.youtube.com/watch?v=0kq99Y8m0gw" target="_blank">
+              https://www.youtube.com/watch?v=0kq99Y8m0gw
+            </a>
+          </li>
+        </ul>
+
+        <h2>Проектирование</h2>
+        <ul>
+          <li>
+            <strong>Проектирование масштабного сервиса:</strong>{' '}
+            <a href="https://www.youtube.com/watch?v=mr3pywHOz7I" target="_blank">
+              https://www.youtube.com/watch?v=mr3pywHOz7I
+            </a>
+          </li>
+        </ul>
+      </>
     </Layout>
   );
 };
