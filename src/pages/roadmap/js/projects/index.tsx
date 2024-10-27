5527a0f9fee3f904b99dcf0c7bb3524b429b0e43
Zrossiz <soon.pluxury@mail.ru>
2024-10-27 17:16:07 +0300

    feat: added projects page for js roadmap

--- src/pages/roadmap/js/projects/index.tsx
+++ src/pages/roadmap/js/projects/index.tsx
@@ -1,9 +1,73 @@
 import { Layout } from '@/layout/Layout';
+import Link from 'next/link';
+import styles from '../../../../styles/Projects.module.scss';
 
 const JsProjectsPage = () => {
   return (
     <Layout>
-      <div>js projects</div>
+      <>
+        <h1>Проекты</h1>
+
+        <div className={styles.content}>
+          <p>
+            Проекты разделены на три уровня (по объему работы) <br />
+            Советую выполнять проекты в указанном порядке
+          </p>
+
+          <div>
+            <h2>Маленькие</h2>
+
+            <ul>
+              <li>
+                <Link href="/roadmap/js/projects/hangman">Виселица</Link>
+              </li>
+              <li>
+                <Link href="/roadmap/js/projects/web-server">Веб сервер</Link>
+              </li>
+              <li>
+                <Link href="/roadmap/js/projects/password-generator">Генератор паролей</Link>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>Средние</h2>
+
+            <ul>
+              <li>
+                <Link href="/roadmap/js/projects/todo">Планировщик задач</Link>
+              </li>
+              <li>
+                <Link href="/roadmap/js/projects/metrics">Метрики</Link>
+              </li>
+              <li>
+                <Link href="/roadmap/js/projects/cloud-storage">Облачное хранилище</Link>
+              </li>
+              <li>
+                <Link href="/roadmap/js/projects/recommendation-engine">
+                  Движок для рекоммендаций
+                </Link>
+              </li>
+              <li>
+                <Link href="/roadmap/js/projects/image-processing">Обработчик изображений</Link>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h2>Большие</h2>
+
+            <ul>
+              <li>
+                <Link href="/roadmap/go/projects/e-commerce">Интернет магазин</Link>
+              </li>
+              <li>
+                <Link href="/roadmap/go/projects/booking-system">Система бронирования</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </>
     </Layout>
   );
 };
